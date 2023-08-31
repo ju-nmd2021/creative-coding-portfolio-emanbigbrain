9d845c92e15e5ef2533bb9cb21395de657390652
Emmanuel Lutterodt <emmanuel.e.lutterodt@gmail.com>
2023-08-31 16:09:59 +0200

    Yellow Tron Style Hills fix

---
+++ experiments/staticGenerative.js
@@ -0,0 +1,36 @@
+//fractal N-Gon by https://editor.p5js.org/MaximSchoemaker/sketches/-bcPIqdon
+
+let widthsquare = 25;
+let heightsquare = 25;
+let x = innerWidth/2;
+let y = innerHeight/2;
+let weirdThing = y + 7;
+
+function setup() {
+  createCanvas(600, 600);
+  background(0, 0, 0);
+
+}
+
+function draw() {
+noFill();
+stroke(255, 255, 0);
+
+  for (let i = 1; i <= 1; i++) {
+    ellipse(x, y, widthsquare, heightsquare);
+    widthsquare = widthsquare + 20;
+    heightsquare = y; 
+    y = y + 7;
+  }
+
+  for (let i = 1; i <= 1; i++) {
+    ellipse(20, y, widthsquare, heightsquare);
+    widthsquare = widthsquare + 20;
+    heightsquare = y; 
+    y = y + 7;
+  }
+
+  
+  
+}
+ 
